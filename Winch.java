0265109c44bf855802c4541d4c4c9d273194e376
Erin A Boyd <eboyd@redhat.com>
2017-02-18 15:25:00 -0700

    Create Winch.java

---
+++ Winch.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team6400.robot.subsystems;
+
+import edu.wpi.first.wpilibj.RobotDrive
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ *
+ */
+public class Winch extends Subsystem {
+    
+	public final Winch winch = new RobotDrive(0); //whatever you already had here
+     // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+    	LiveWindow.addActuator("Winch", "Winch",winch);
+    }
+    
+    public void winchStart() {
+    	motor.set(1);
+    }
+    
+    public void winchStop() {
+    	motor.set(-1);
+    }
+}
